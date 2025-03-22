985a5ba2c4279f1eaa6845c1c75b6905df2c664e
Satya Mishra <Satya.Mishra@flydocs.aero>
2025-03-23 01:58:30 +0530

    init: husky fixes in package

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,21 +2,21 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-    2,
+      2,
       'always',
       [
-        'feat',     // New feature
-        'fix',      // Bug fix
-        'docs',     // Documentation update
-        'style',    // Code style changes (formatting, no logic change)
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation update
+        'style', // Code style changes (formatting, no logic change)
         'refactor', // Refactoring code (no new feature, no bug fix)
-        'perf',     // Performance improvements
-        'test',     // Adding or updating tests
-        'chore',    // Maintenance tasks
-        'ci',       // Continuous Integration changes
-        'build',    // Build-related changes
-        'revert',   // Reverting a previous commit
-        'init'      // Initial commit
+        'perf', // Performance improvements
+        'test', // Adding or updating tests
+        'chore', // Maintenance tasks
+        'ci', // Continuous Integration changes
+        'build', // Build-related changes
+        'revert', // Reverting a previous commit
+        'init', // Initial commit
       ],
     ],
     'scope-empty': [2, 'never'],
