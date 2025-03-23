1e75cd9cf55b4d7430127eba0d8d6af3ed42600f
Satya Mishra <Satya.Mishra@flydocs.aero>
2025-03-23 13:09:30 +0530

    init: route setup fixes

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import authRoutes from './modules/users/routes/auth.route';
 
 const router = express.Router();
-const app = express();
 
 // Authentication routes
-app.use('/auth', authRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,8 @@ import 'module-alias/register';
 import connectDB from './config/db';
 import env from './config/env';
 import allRoutes from './routes';
-import { globalPrefixMiddleware } from './shared/middleware/global-prefix.middleware';
+
+const globalPrefix = `/${env.API_PREFIX}/${env.API_VERSION}`;
 
 function bootstrap() {
   dotenv.config();
@@ -16,19 +17,16 @@ function bootstrap() {
   app.use(express.json());
   app.use(cors());
 
-  /** @middleware globalPrefixMiddleware will add the prefix, versioning via env or default /api/v1/ */
-  app.use(globalPrefixMiddleware);
-
-  /** TODO: This is for the testing */
-  app.use('/', (_req, res) => {
+  /** TODO: This is for testing - Returns a simple message */
+  app.get('/', (_req, res) => {
     res.status(200).json({ message: 'Welcome to the LMS' });
   });
 
   /** @middleware allRoutes will add all the routes */
-  app.use(allRoutes);
+  app.use(globalPrefix, allRoutes);
 
   const PORT = env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
 
 bootstrap();
